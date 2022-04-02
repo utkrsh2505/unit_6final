2b4fc93645c09d3d48473e1e13adcdbae6b05f24
Utkarsh Kumar <kumarutkarsh2505@gmail.com>
2022-04-02 16:33:19 +0530

    book

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     author : {type : String},
-    password : {type : String}
+    password : {type : String},
+    email  : {type : String}
 })
 
 const User = mongoose.model("user", userSchema);
